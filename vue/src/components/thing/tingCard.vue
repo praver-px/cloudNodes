d596b26e8a00de3939eb0cb63b92368bf6d8ad90
praver <praverpx@gamil.com>
2023-07-14 22:17:36 +0800

    小记展示卡片组件化

---
+++ vue/src/components/thing/tingCard.vue
@@ -0,0 +1,157 @@
+<script setup>
+import {
+  ArrowCircleDownRound,
+  ArrowCircleUpRound,
+  ArrowUpwardRound,
+  DeleteOutlineRound,
+  EditNoteRound
+} from "@vicons/material";
+import {useThemeStore} from "@/stores/themeStore";
+import {computed, ref} from "vue";
+import {getUserToken, loginInvalid} from "@/utils/userLoginUtil";
+import {noteBaseRequest} from "@/request/noteRequest";
+import {useLoadingBar, useMessage} from 'naive-ui'
+
+const message = useMessage()
+const loadingBar = useLoadingBar()
+
+//主题对象
+const themeStore = useThemeStore()
+const {isDarkTheme} = themeStore
+
+const propsData = defineProps({
+  id: {type: Number, required: true},// 编号
+  finished: {type: Boolean, default: false},// 是否已完成，默认未完成
+  title: {type: String, required: true},// 小记标题
+  top: {type: Boolean, default: false}, //是否置顶
+  tags: {type: Array, required: ['暂无标签']},// 小记标签
+  time: {type: String, required: true},// 更新时间
+})
+
+//自定义事件
+const emits = defineEmits(['changeStatus']);
+
+const thingFinishShadowColor = computed(() => {
+  return "#363433"
+})
+//置顶按钮是否被禁用
+const topBtnDisabled = ref(false)
+const thingCardTopIconText = computed(() => {
+  if (propsData.top) {
+    return {
+      icon: ArrowCircleDownRound,
+      text: '取消置顶'
+    }
+  } else {
+    return {
+      icon: ArrowCircleUpRound,
+      text: '置顶'
+    }
+  }
+})
+const topThing = async (isTop) => {
+  const userToken = await getUserToken();
+  loadingBar.start()
+  topBtnDisabled.value = true
+  const {data: responseData} = await noteBaseRequest.get(
+      "/thing/top",
+      {
+        params: {isTop, thingId: propsData.id},
+        headers: {userToken}
+      }
+  ).catch(() => {
+    loadingBar.error()
+    topBtnDisabled.value = false
+    throw message.error(isTop ? "置顶失败！" : "取消置顶失败！")
+  })
+  topBtnDisabled.value = false
+  if (responseData.success) {
+    loadingBar.finish()
+    message.success(responseData.message)
+    emits('changeStatus')
+  } else {
+    loadingBar.error();
+    message.error(responseData.message)
+    if (responseData.code === "L_008") {
+      loginInvalid(true)
+    }
+  }
+}
+</script>
+
+<template>
+  <n-card
+      :class="{'thing-card-finished': finished}"
+      size="small"
+      embedded
+      :bordered="isDarkTheme"
+      :segmented="{'content':'soft'}"
+      :title="title"
+      style="min-width: 230px;">
+    <template #header-extra>
+      <n-popover>
+        <template #trigger>
+          <n-button type="error" text style="margin-left: 16px">
+            <n-icon :size="18" :component="DeleteOutlineRound"/>
+          </n-button>
+        </template>
+        删除
+      </n-popover>
+
+
+      <n-popover>
+        <template #trigger>
+          <!-- !! 数值类型 -> boolean 类型 -->
+          <n-button :disabled="topBtnDisabled" text style="margin-left: 6px" @click="topThing(!top)">
+            <n-icon :size="18" :component="thingCardTopIconText.icon"/>
+          </n-button>
+        </template>
+        {{ thingCardTopIconText.text }}
+      </n-popover>
+
+      <n-popover>
+        <template #trigger>
+          <n-button text style="margin-left: 6px">
+            <n-icon :size="18" :component="EditNoteRound"/>
+          </n-button>
+        </template>
+        编辑
+      </n-popover>
+
+    </template>
+    <template #default>
+      <n-space>
+        <n-tag v-for="tag in tags" size="small" :bordered="false">{{ tag }}</n-tag>
+
+      </n-space>
+    </template>
+    <template #footer>
+      <n-space align="center" size="small">
+        <n-tag
+            :color="{color:'rgba(0 0 0 0)',textColor: '#18A058'}"
+            v-if="top"
+            round
+            size="small"
+            :bordered="false">
+          <n-icon size="18" :component="ArrowUpwardRound"/>
+        </n-tag>
+        <n-divider v-if="top" vertical/>
+        <n-text depth="3">{{ time }}</n-text>
+      </n-space>
+    </template>
+  </n-card>
+</template>
+<style scoped>
+.n-card.thing-card-finished:after {
+  position: absolute;
+  content: '';
+  width: 100px;
+  height: 100px;
+  left: 40px;
+  transform: translateY(50px);
+  background-image: url("@/assets/finish.png");
+  background-size: 100px 100px;
+  background-repeat: no-repeat;
+  filter: drop-shadow(5px 3px 2px v-bind(thingFinishShadowColor));
+}
+</style>
--- vue/src/views/thing/indexView.vue
+++ vue/src/views/thing/indexView.vue
@@ -1,76 +1,26 @@
 <script setup>
-import {
-  ArrowCircleDownRound,
-  ArrowCircleUpRound,
-  DeleteOutlineRound,
-  EditNoteRound,
-  ArrowUpwardRound,
-  ContentPasteOffRound
-} from '@vicons/material'
-import {computed, ref} from 'vue'
+import {ContentPasteOffRound} from '@vicons/material'
+import {ref} from 'vue'
 import {useThemeStore} from "@/stores/themeStore";
 import {storeToRefs} from "pinia";
 import {getUserToken, loginInvalid} from '@/utils/userLoginUtil'
 import {useLoadingBar, useMessage} from 'naive-ui'
 import {noteBaseRequest} from "@/request/noteRequest";
+import ThingCard from '@/components/thing/tingCard.vue'
 
 const message = useMessage()
 const loadingBar = useLoadingBar()
 
 const things = ref([])
 
-const thingCardTopIconText = top => {
-  if (top) {
-    return {
-      icon: ArrowCircleDownRound,
-      text: '取消置顶'
-    }
-  } else {
-    return {
-      icon: ArrowCircleUpRound,
-      text: '置顶'
-    }
-  }
-}
 
 const themeStore = useThemeStore()
 const {isDarkTheme} = storeToRefs(themeStore)
 
-const thingFinishShadowColor = computed(() => {
-  return isDarkTheme.value ? "#363433" : "#bdaead"
-})
-const loading = ref(true)
-const topThing = async (isTop, thingId, index) => {
-  const userToken = await getUserToken();
-  loadingBar.start()
-  const thing = things.value[index]
-  thing.toTop = true
-  const {data: responseData} = await noteBaseRequest.get(
-      "/thing/top",
-      {
-        params: {isTop, thingId},
-        headers: {userToken}
-      }
-  ).catch(() => {
-    loadingBar.error()
-    thing.toTop = false
-    throw message.error(isTop ? "置顶失败！" : "取消置顶失败！")
-  })
 
-  if (responseData.success) {
-    loadingBar.finish()
-    message.success(responseData.message)
-    getThingList(false)
-  } else {
-    loadingBar.error();
-    thing.toTop = false
-    message.error(responseData.message)
-    if (responseData.code === "L_008") {
-      loginInvalid(true)
-    }
-  }
-}
-const getThingList = async (isUpdateLoading) => {
+const loading = ref(true)
+
+const getThingList = async () => {
   //判断登录状态
   const userToken = await getUserToken();
   //发送获取小记列表请求
@@ -89,7 +39,7 @@ const getThingList = async (isUpdateLoading) => {
   if (responseData.success) {
     loadingBar.finish()
     things.value = responseData.data
-    if (isUpdateLoading) loading.value = false
+    loading.value = false
   } else {
     loadingBar.error();
     message.error(responseData.message)
@@ -99,7 +49,7 @@ const getThingList = async (isUpdateLoading) => {
   }
 }
 
-getThingList(true)
+getThingList()
 </script>
 
 <template>
@@ -144,68 +94,16 @@ getThingList(true)
       </n-space>
       <!--      具体小记信息-->
       <n-space v-else-if="!loading && things.length >0">
-        <n-card
-            :class="{'thing-card-finished': t.finished}"
-            size="small"
-            v-for="t in things"
-            :key="t.id"
-            embedded
-            :bordered="isDarkTheme"
-            :segmented="{'content':'soft'}"
-            :title="t.title"
-            style="min-width: 230px;">
-          <template #header-extra>
-            <n-popover>
-              <template #trigger>
-                <n-button type="error" text style="margin-left: 16px">
-                  <n-icon :size="18" :component="DeleteOutlineRound"/>
-                </n-button>
-              </template>
-              删除
-            </n-popover>
-
-
-            <n-popover>
-              <template #trigger>
-                <!-- !! 数值类型 -> boolean 类型 -->
-                <n-button :disabled="t.toTop" text style="margin-left: 6px" @click="topThing(!!!t.top,t.id,index)">
-                  <n-icon :size="18" :component="thingCardTopIconText(t.top).icon"/>
-                </n-button>
-              </template>
-              {{ thingCardTopIconText(t.top).text }}
-            </n-popover>
-
-            <n-popover>
-              <template #trigger>
-                <n-button text style="margin-left: 6px">
-                  <n-icon :size="18" :component="EditNoteRound"/>
-                </n-button>
-              </template>
-              编辑
-            </n-popover>
-
-          </template>
-          <template #default>
-            <n-space>
-              <n-tag v-for="tag in t.tags.split(',')" :key="tag" size="small" :bordered="false">{{ tag }}</n-tag>
-
-            </n-space>
-          </template>
-          <template #footer>
-            <n-space align="center" size="small">
-              <n-tag
-                  :color="{color:'rgba(0 0 0 0)',textColor: '#18A058'}"
-                  v-if="t.top"
-                  round
-                  size="small"
-                  :bordered="false">
-                <n-icon size="18" :component="ArrowUpwardRound"/>
-              </n-tag>
-              <n-divider v-if="t.top" vertical/>
-              <n-text depth="3">{{ t.updateTime }}</n-text>
-            </n-space>
-          </template>
-        </n-card>
+        <thing-card
+            v-for="thing in things"
+            :key="thing.id"
+            :id="thing.id"
+            :title="thing.title"
+            :finished="!!thing.finished"
+            :top="!!thing.top"
+            :tags="thing.tags.split(',')"
+            :time="thing.updateTime"
+            @change-status="getThingList(false)"/>
       </n-space>
       <!--      暂无内容-->
       <n-empty v-else style="margin: 20px auto;" description="暂无小记数据，灵光一现，小记一下？" :size="'huge'">
@@ -219,17 +117,3 @@ getThingList(true)
     </n-card>
   </n-layout>
 </template>
-<style scoped>
-.n-card.thing-card-finished:after {
-  position: absolute;
-  content: '';
-  width: 100px;
-  height: 100px;
-  left: 40px;
-  transform: translateY(50px);
-  background-image: url("@/assets/finish.png");
-  background-size: 100px 100px;
-  background-repeat: no-repeat;
-  filter: drop-shadow(5px 3px 2px v-bind(thingFinishShadowColor));
-}
-</style>
