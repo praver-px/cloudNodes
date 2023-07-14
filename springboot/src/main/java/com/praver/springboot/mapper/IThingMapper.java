f274e3192c2dd7485f20720c0e844e870ca2be4d
praver <praverpx@gamil.com>
2023-07-14 16:20:31 +0800

    登录注册退出登录功能完善

---
+++ springboot/src/main/java/com/praver/springboot/mapper/IThingMapper.java
@@ -0,0 +1,12 @@
+package com.praver.springboot.mapper;
+
+
+import com.mybatisflex.core.BaseMapper;
+import com.praver.springboot.entity.Thing;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface IThingMapper extends BaseMapper<Thing> {
+}
